ee396ae7eca93b3f9a58b5295ee87360c8b60398
Gena <krymov.gena2024@gmail.com>
2026-09-10 12:45:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+from app.errors import (VaccineError,
+                        NotWearingMaskError
+                        )
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
